1a7b49c494d75e333c69499a278ee796547b0831
Yusuf Fahmi Adiputera <yfa.adiputera@gmail.com>
2018-03-21 12:01:43 +0700

    role + user

--- src/main/java/com/xsis/batch137/model/Role.java
+++ src/main/java/com/xsis/batch137/model/Role.java
@@ -1,8 +1,22 @@
 package com.xsis.batch137.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Role {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 }
