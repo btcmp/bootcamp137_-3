1aa43c14d043087742e0f85bde20a8fdd5ae683d
user <user@YOSIA-PC2>
2018-03-21 11:28:00 +0700

    Created New

---
+++ src/main/java/com/xsis/batch137/model/Province.java
@@ -0,0 +1,98 @@
+package com.xsis.batch137.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="PROVINCE")
+public class Province {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private String id;
+	@NotNull
+	@NotEmpty
+	@Size(max=50)
+	private String name;
+	@Column(name="created_by")
+	private long createdBy;
+	@Column(name="created_on")
+	private Date createdOn;
+	@Column(name="modified_by")
+	private long modifiedBy;
+	@Column(name="modified_on")
+	private Date modifiedOn;
+	@NotNull
+	@NotEmpty
+	private boolean active;
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="province", cascade=CascadeType.ALL, orphanRemoval=true)
+	private List<Region> regions;
+	
+	
+	
+	public List<Region> getRegions() {
+		return regions;
+	}
+	public void setRegions(List<Region> regions) {
+		this.regions = regions;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(long createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public long getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(long modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Date getModifiedOn() {
+		return modifiedOn;
+	}
+	public void setModifiedOn(Date modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
+}
