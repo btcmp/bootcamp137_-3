5313b6ae0a93e89e132219d3ae72f90593f27ef2
Yusuf Fahmi Adiputera <yfa.adiputera@gmail.com>
2018-03-21 14:15:22 +0700

    user

--- src/main/java/com/xsis/batch137/model/User.java
+++ src/main/java/com/xsis/batch137/model/User.java
@@ -1,10 +1,19 @@
 package com.xsis.batch137.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="user_137")
@@ -13,4 +22,39 @@ public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private long id;
+	
+	@Size(max=50)
+	@Column(nullable=false)
+	private String username;
+	
+	@Column(nullable=false)
+	@Size(max=250)
+	private String password;
+	
+	@ManyToOne
+	@JoinColumn(name="role_id", nullable=false)
+	private Role role;
+	
+	@OneToOne
+	@JoinColumn(name="employee_id", nullable=false)
+	private Employee employee;
+	
+	@ManyToOne
+	@JoinColumn(name="created_by", nullable=true)
+	private User createdBy;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="created_on", nullable=true)
+	private Date createdOn;
+	
+	@ManyToOne
+	@JoinColumn(name="modified_by", nullable=true)
+	private User modifiedBy;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="modified_on", nullable=true)
+	private Date modifiedOn;
+	
+	@Column(nullable=false)
+	private boolean active;
 }
