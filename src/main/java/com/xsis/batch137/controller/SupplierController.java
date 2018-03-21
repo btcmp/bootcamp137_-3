42894df3b437ce633cf27879454571a79dabd324
user <user@YOSIA-PC2>
2018-03-21 15:57:30 +0700

    Supplier Controller added

---
+++ src/main/java/com/xsis/batch137/controller/SupplierController.java
@@ -0,0 +1,58 @@
+package com.xsis.batch137.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.xsis.batch137.model.Supplier;
+import com.xsis.batch137.service.SupplierService;
+
+@Controller
+@RequestMapping("/supplier")
+public class SupplierController {
+
+	@Autowired
+	SupplierService supplierService;
+	
+	@RequestMapping
+	public String view(Model model) {
+		List<Supplier> sups = supplierService.selectAll();
+		model.addAttribute("suppliers", sups);
+		return "supplier-view";
+	}
+	
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void save (@RequestBody Supplier supplier) {
+		supplierService.save(supplier);
+	}
+	
+	@RequestMapping(value="/take", method=RequestMethod.GET)
+	public void getOne(long id, Model model) {
+		Supplier supplier = supplierService.getOne(id);
+		model.addAttribute("supplier", supplier);
+	}
+	
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void update(@RequestBody Supplier supplier) {
+		supplierService.update(supplier);
+	}
+	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
+	public void delete(@PathVariable long id) {
+		supplierService.delete(id);
+	}
+}
