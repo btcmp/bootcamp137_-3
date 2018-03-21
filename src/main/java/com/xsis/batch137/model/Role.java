836921ba4ea796d08a4ac3b1bb881f30b45702df
Yusuf Fahmi Adiputera <yfa.adiputera@gmail.com>
2018-03-21 14:28:36 +0700

    Role

--- src/main/java/com/xsis/batch137/model/Role.java
+++ src/main/java/com/xsis/batch137/model/Role.java
@@ -1,9 +1,17 @@
 package com.xsis.batch137.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Role {
@@ -11,6 +19,32 @@ public class Role {
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private long id;
+	
+	@Column(nullable=true)
+	@Size(max=50)
+	private String name;
+
+	@Column(nullable=true)
+	@Size(max=250)
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="created_by", nullable=true)
+	private User createdBy;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(nullable=true, name="created_on")
+	private Date createdOn;
+	
+	@JoinColumn(name="modified_by",nullable=true)
+	private User modifiedBy;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(nullable=true, name="modified_on")
+	private Date modifiedOn;
+	
+	@Column(nullable=false)
+	private boolean active;
 
 	public long getId() {
 		return id;
@@ -18,5 +52,61 @@ public class Role {
 
 	public void setId(long id) {
 		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public User getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(User modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public Date getModifiedOn() {
+		return modifiedOn;
+	}
+
+	public void setModifiedOn(Date modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
 	}
 }
