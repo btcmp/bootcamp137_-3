dcb6a77d3db047273e3ba3dcab5776cda81bd0d8
user <user@YOSIA-PC2>
2018-03-21 10:59:25 +0700

    Created New

---
+++ src/main/java/com/xsis/batch137/model/Supplier.java
@@ -0,0 +1,134 @@
+package com.xsis.batch137.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name="SUPPLIER")
+public class Supplier {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	@NotNull
+	@NotEmpty
+	@Size(max=50)
+	private String name;
+	private String address;
+	@Size(max=16)
+	private String phone;
+	@Email
+	@Size(max=50)
+	private String email;
+	private long provinceId;
+	private long regionId;
+	private long districtId;
+	@Size(max=6)
+	private String postalCode;
+	private long createdBy;
+	private Date createdOn;
+	private long modifiedBy;
+	private Date modifiedOn;
+	@NotNull
+	@NotEmpty
+	private boolean active;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getProvinceId() {
+		return provinceId;
+	}
+	public void setProvinceId(long provinceId) {
+		this.provinceId = provinceId;
+	}
+	public long getRegionId() {
+		return regionId;
+	}
+	public void setRegionId(long regionId) {
+		this.regionId = regionId;
+	}
+	public long getDistrictId() {
+		return districtId;
+	}
+	public void setDistrictId(long districtId) {
+		this.districtId = districtId;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public long getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(long createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public long getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(long modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Date getModifiedOn() {
+		return modifiedOn;
+	}
+	public void setModifiedOn(Date modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
+}
