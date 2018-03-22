ae4106a598db89f887d41078490d0d34a47b338b
Yusuf Fahmi Adiputera <yfa.adiputera@gmail.com>
2018-03-22 09:36:38 +0700

    [DAO] ROLE DAO

---
+++ src/main/java/com/xsis/batch137/dao/RoleDao.java
@@ -0,0 +1,10 @@
+package com.xsis.batch137.dao;
+
+import java.util.List;
+
+import com.xsis.batch137.model.Role;
+
+public interface RoleDao {
+
+	List<Role> selectAll();
+}
---
+++ src/main/java/com/xsis/batch137/dao/RoleDaoImpl.java
@@ -0,0 +1,22 @@
+package com.xsis.batch137.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.xsis.batch137.model.Role;
+
+@Repository
+public class RoleDaoImpl implements RoleDao {
+
+	@Autowired
+	SessionFactory sessionFactory;
+	public List<Role> selectAll() {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		return session.createCriteria(Role.class).list();
+	}
+}
