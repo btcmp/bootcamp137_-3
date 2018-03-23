d90273b3faf5736bc53d070239cf390099314e3c
user <user@YOSIA-PC2>
2018-03-23 12:21:19 +0700

    [Class] Add  2 New Class

---
+++ src/main/java/com/xsis/batch137/model/Adjustment.java
@@ -0,0 +1,103 @@
+package com.xsis.batch137.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name="adjustment")
+public class Adjustment {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	
+	private String notes;
+	
+	@NotNull
+	@NotEmpty
+	@Column(nullable=false)
+	@Size(max=20)
+	private String status;
+	
+	@JoinColumn(name="created_by")
+	@ManyToOne
+	private User createdBy;
+	
+	@Column(name="created_on")
+	private Date createdOn;
+	
+	@JoinColumn(name="modified_by")
+	@ManyToOne
+	private User modifiedBy;
+	
+	@Column(name="modified_on")
+	private Date modifiedOn;
+	
+	@ManyToOne
+	@NotNull
+	@NotEmpty
+	@JoinColumn(name="outlet_id", nullable=false)
+	private Outlet outlet;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public User getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public User getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(User modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Date getModifiedOn() {
+		return modifiedOn;
+	}
+	public void setModifiedOn(Date modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+	public Outlet getOutlet() {
+		return outlet;
+	}
+	public void setOutlet(Outlet outlet) {
+		this.outlet = outlet;
+	}
+
+}
---
+++ src/main/java/com/xsis/batch137/model/AdjustmentDetail.java
@@ -0,0 +1,96 @@
+package com.xsis.batch137.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="adjustment_detail")
+public class AdjustmentDetail {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	@Column(name="in_stock")
+	private int inStock;
+	@Column(name="actual_stock")
+	private int actualStock;
+	@ManyToOne
+	@JoinColumn(name="created_by")
+	private User createdBy;
+	@Column(name="created_on")
+	private Date createdOn;
+	@ManyToOne
+	@JoinColumn(name="modified_by")
+	private User modifiedBy;
+	@Column(name="modified_on")
+	private Date modifiedOn;
+	@ManyToOne
+	private Adjustment adjustment;
+	@ManyToOne
+	private ItemVariant variant;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getInStock() {
+		return inStock;
+	}
+	public void setInStock(int inStock) {
+		this.inStock = inStock;
+	}
+	public int getActualStock() {
+		return actualStock;
+	}
+	public void setActualStock(int actualStock) {
+		this.actualStock = actualStock;
+	}
+	public User getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public User getModifiedBy() {
+		return modifiedBy;
+	}
+	public void setModifiedBy(User modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+	public Date getModifiedOn() {
+		return modifiedOn;
+	}
+	public void setModifiedOn(Date modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
+	public Adjustment getAdjustment() {
+		return adjustment;
+	}
+	public void setAdjustment(Adjustment adjustment) {
+		this.adjustment = adjustment;
+	}
+	public ItemVariant getVariant() {
+		return variant;
+	}
+	public void setVariant(ItemVariant variant) {
+		this.variant = variant;
+	}
+	
+}
