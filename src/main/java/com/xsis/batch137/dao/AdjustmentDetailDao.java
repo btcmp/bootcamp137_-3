795093a233edc515a061174fafb78532018282b5
user <yosia.manurun@gmail.com>
2018-03-23 14:58:47 +0700

    [Class] Add AdjustmentDetailDao

---
+++ src/main/java/com/xsis/batch137/dao/AdjustmentDetailDao.java
@@ -0,0 +1,19 @@
+package com.xsis.batch137.dao;
+
+import java.util.List;
+
+import com.xsis.batch137.model.AdjustmentDetail;
+
+
+public interface AdjustmentDetailDao {
+
+	public void save(AdjustmentDetail adjDetail);
+	
+	public void update(AdjustmentDetail adjDetail);
+	
+	public void delete(AdjustmentDetail adjDetail);
+	
+	public List<AdjustmentDetail> selectAll();
+	
+	public AdjustmentDetail getOne(long id);
+}
---
+++ src/main/java/com/xsis/batch137/dao/AdjustmentDetailDaoImpl.java
@@ -0,0 +1,50 @@
+package com.xsis.batch137.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.xsis.batch137.model.AdjustmentDetail;
+
+@Repository
+public class AdjustmentDetailDaoImpl {
+
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	public void save(AdjustmentDetail adjDetail) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		session.save(adjDetail);
+		session.flush();
+	}
+
+	public void update(AdjustmentDetail adjDetail) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		session.update(adjDetail);
+		session.flush();
+	}
+
+	public void delete(AdjustmentDetail adjDetail) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		session.delete(adjDetail);
+		session.flush();
+	}
+
+	public List<AdjustmentDetail> selectAll() {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		return session.createCriteria(AdjustmentDetail.class).list();
+	}
+
+	public AdjustmentDetail getOne(long id) {
+		// TODO Auto-generated method stub
+		Session session = sessionFactory.getCurrentSession();
+		return session.get(AdjustmentDetail.class, id);
+	}
+}
