8dacc9a8669980471ee45ebb4c131694767608a6
user <user@YOSIA-PC2>
2018-03-23 12:29:26 +0700

    [Controller] Add New Adjustment Controller

---
+++ src/main/java/com/xsis/batch137/controller/AdjustmentController.java
@@ -0,0 +1,11 @@
+package com.xsis.batch137.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("adjustment")
+public class AdjustmentController {
+
+	
+}
