f6d502b91fcdea5962aaaf2ff6cb13280f30c5f4
user <user@YOSIA-PC2>
2018-03-23 13:55:12 +0700

    [Class] Add New Adjustment History

---
+++ src/main/java/com/xsis/batch137/model/AdjustmentHistory.java
@@ -0,0 +1,73 @@
+package com.xsis.batch137.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name="adjustment_history")
+public class AdjustmentHistory {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	@NotNull
+	@NotEmpty
+	@Column(nullable=false)
+	@Size(max=20)
+	private String status;
+	@ManyToOne
+	@JoinColumn(name="created_by", nullable=false)
+	private User createdBy;
+	@Column(name="created_on")
+	private Date createdOn;
+	@ManyToOne
+	@JoinColumn(name="adjustment_id")
+	private Adjustment adjustment;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public User getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	public Adjustment getAdjustment() {
+		return adjustment;
+	}
+	public void setAdjustment(Adjustment adjustment) {
+		this.adjustment = adjustment;
+	}
+	
+	
+}
