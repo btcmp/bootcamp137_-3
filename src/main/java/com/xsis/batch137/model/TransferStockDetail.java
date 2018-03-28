f9f6c6e0fca11cdd9a8d825b1548cc66d62672b1
Yusuf Fahmi Adiputera <yfa.adiputera@gmail.com>
2018-03-28 09:48:08 +0700

    [APPCONTEXT] app context + transforstock

--- src/main/java/com/xsis/batch137/model/TransferStockDetail.java
+++ src/main/java/com/xsis/batch137/model/TransferStockDetail.java
@@ -3,14 +3,18 @@ package com.xsis.batch137.model;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import com.sun.istack.NotNull;
 
+@Entity
+@Table(name="pos_t_transfer_stock_detail")
 public class TransferStockDetail {
 	//
 	public TransferStockDetail() {
