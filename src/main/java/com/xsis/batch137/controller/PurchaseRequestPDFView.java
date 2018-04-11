8eb461525fed3a3a5d0cfc578b3a53cb3676383c
Yusuf Fahmi Adiputera <yfa.adiputera@gmail.com>
2018-04-11 15:50:23 +0700

    [PDF] PR

--- src/main/java/com/xsis/batch137/controller/PurchaseRequestPDFView.java
+++ src/main/java/com/xsis/batch137/controller/PurchaseRequestPDFView.java
@@ -31,50 +31,50 @@ public class PurchaseRequestPDFView extends AbstractPdfView {
 	protected void buildPdfDocument(Map<String, Object> model, Document doc, PdfWriter writer, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-			
-			doc.setPageSize(PageSize.A4.rotate());
-			doc.newPage();
-			
-			List<PurchaseRequest> prs = (List<PurchaseRequest>) model.get("pr");
-			Outlet outlet = null;
-			for(PurchaseRequest pr : prs) {
-				outlet = pr.getOutlet();
-			}
-			
-			
-		   	PdfPTable table = new PdfPTable(5);
-			table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-			table.getDefaultCell().setVerticalAlignment(Element.ALIGN_MIDDLE);
+	
+		doc.setPageSize(PageSize.A4.rotate());
+		doc.newPage();
+		
+		List<PurchaseRequest> prs = (List<PurchaseRequest>) model.get("pr");
+		Outlet outlet = null;
+		for(PurchaseRequest pr : prs) {
+			outlet = pr.getOutlet();
+		}
+		
+		
+	   	PdfPTable table = new PdfPTable(5);
+		table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+		table.getDefaultCell().setVerticalAlignment(Element.ALIGN_MIDDLE);
 
-			Font font = new Font(Font.BOLD, 16);
-			Chunk title = new Chunk("Purchase Request", font);
-			Paragraph judul = new Paragraph(title);
-			judul.setAlignment(Element.ALIGN_CENTER);
-			doc.add(judul);
-			doc.add(new Paragraph(new Chunk("  ")));
-			
-			Chunk outl = new Chunk("Outlet : "+outlet.getName());
-			Paragraph parout = new Paragraph(outl);
-			parout.setAlignment(Element.ALIGN_LEFT);
-			doc.add(parout);
-			doc.add(new Paragraph(new Chunk("  ")));
-			
-			table.addCell("No.");
-			table.addCell("Created On");
-			table.addCell("PR No");
-			table.addCell("Notes");
-			table.addCell("Status");
-			
-			int i = 0;
-			for (PurchaseRequest pr : prs) {
-				String created = pr.getCreatedOn().toString().split("\\.")[0];
-				table.addCell(String.valueOf(i));
-				table.addCell(created);
-				table.addCell(pr.getPrNo()); 
-				table.addCell(pr.getNotes()); 
-				table.addCell(pr.getStatus()); 
-				i++;
-			}
-			doc.add(table);
+		Font font = new Font(Font.BOLD, 16);
+		Chunk title = new Chunk("Purchase Request", font);
+		Paragraph judul = new Paragraph(title);
+		judul.setAlignment(Element.ALIGN_CENTER);
+		doc.add(judul);
+		doc.add(new Paragraph(new Chunk("  ")));
+		
+		Chunk outl = new Chunk("Outlet : "+outlet.getName());
+		Paragraph parout = new Paragraph(outl);
+		parout.setAlignment(Element.ALIGN_LEFT);
+		doc.add(parout);
+		doc.add(new Paragraph(new Chunk("  ")));
+		
+		table.addCell("No.");
+		table.addCell("Created On");
+		table.addCell("PR No");
+		table.addCell("Notes");
+		table.addCell("Status");
+		
+		int i = 1;
+		for (PurchaseRequest pr : prs) {
+			String created = pr.getCreatedOn().toString().split("\\.")[0];
+			table.addCell(String.valueOf(i));
+			table.addCell(created);
+			table.addCell(pr.getPrNo()); 
+			table.addCell(pr.getNotes()); 
+			table.addCell(pr.getStatus()); 
+			i++;
+		}
+		doc.add(table);
 	}
 }
