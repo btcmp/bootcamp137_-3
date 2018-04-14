eda69e2406717bc66cc12ca6f25285f14008de6f
adiputera <yfa.adiputera@gmail.com>
2018-04-14 17:15:54 +0700

    [Service] add count approved po

--- src/main/java/com/xsis/batch137/service/DashboardService.java
+++ src/main/java/com/xsis/batch137/service/DashboardService.java
@@ -1,10 +1,26 @@
 package com.xsis.batch137.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.xsis.batch137.dao.PurchaseOrderDao;
+import com.xsis.batch137.model.PurchaseOrder;
 
 @Service
 @Transactional
 public class DashboardService {
 
+	@Autowired
+	PurchaseOrderDao pod;
+	
+	public int countApprovedPO() {
+		return pod.CountApprovedPo();
+	}
+	
+	public List<PurchaseOrder> getApprovedPO() {
+		return pod.searchPOByStatus("Approved");
+	}
 }
