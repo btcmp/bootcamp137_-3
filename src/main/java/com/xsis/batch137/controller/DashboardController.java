07ae86deed3da9affecabbe92b3ead2b0d338282
adiputera <yfa.adiputera@gmail.com>
2018-04-14 17:16:16 +0700

    [Controller] add count po

--- src/main/java/com/xsis/batch137/controller/DashboardController.java
+++ src/main/java/com/xsis/batch137/controller/DashboardController.java
@@ -1,19 +1,33 @@
 package com.xsis.batch137.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xsis.batch137.model.PurchaseOrder;
+import com.xsis.batch137.service.DashboardService;
 
 @Controller
 @RequestMapping
 public class DashboardController {
 
-	
-	
-	
+	@Autowired
+	DashboardService ds;
 	
 	@RequestMapping("/dashboard")
 	public String index(Model model) {
+		int jmlPO = ds.countApprovedPO();
+		model.addAttribute("jmlPO", jmlPO);
 		return "dashboard";
+	}
+	
+	@RequestMapping("/dashboard/po")
+	public String listApprovedPO(Model model) {
+		List<PurchaseOrder> pos = ds.getApprovedPO();
+		model.addAttribute("pos", pos);
+		return "purchaseOrder/approved";
 	}
 }
