55c311ac5874ea28d10fbb2710a0af1d6fd8df83
adiputera <yfa.adiputera@gmail.com>
2018-04-14 17:51:30 +0700

    [Service] dashboard

--- src/main/java/com/xsis/batch137/service/DashboardService.java
+++ src/main/java/com/xsis/batch137/service/DashboardService.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.xsis.batch137.dao.AdjustmentDao;
 import com.xsis.batch137.dao.PurchaseOrderDao;
 import com.xsis.batch137.dao.SalesOrderDao;
+import com.xsis.batch137.dao.TransferStockDao;
+import com.xsis.batch137.model.Adjustment;
 import com.xsis.batch137.model.PurchaseOrder;
+import com.xsis.batch137.model.TransferStock;
 
 @Service
 @Transactional
@@ -20,6 +24,12 @@ public class DashboardService {
 	@Autowired
 	SalesOrderDao sod;
 	
+	@Autowired
+	AdjustmentDao ad;
+	
+	@Autowired
+	TransferStockDao tsd;
+	
 	public int countApprovedPO() {
 		return pod.CountApprovedPo();
 	}
@@ -30,5 +40,21 @@ public class DashboardService {
 	
 	public int countSalesOrder() {
 		return sod.selectAll().size();
+	}
+	
+	public int countSubmittedAdjustment() {
+		return ad.getSubmitted().size();
+	}
+	
+	public List<Adjustment> getSubmittedAdjustment(){
+		return ad.getSubmitted();
+	}
+	
+	public int countSubmittedTs() {
+		return tsd.getSubmitted().size();
+	}
+	
+	public List<TransferStock> getSubmittedTs(){
+		return tsd.getSubmitted();
 	}
 }
