fbb2dcf25c6781705ea8dbef2c42aa920ed83704
adiputera <yfa.adiputera@gmail.com>
2018-04-14 16:51:43 +0700

    [Service] Dashboard

---
+++ src/main/java/com/xsis/batch137/service/DashboardService.java
@@ -0,0 +1,10 @@
+package com.xsis.batch137.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class DashboardService {
+
+}
